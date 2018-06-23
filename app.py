291c22d86fe031d7d0978081f406be1fdfe02ee8
Craig <35710823+CTopham@users.noreply.github.com>
2018-06-22 20:37:33 -0400

    Adding updates

--- app.py
+++ app.py
@@ -23,11 +23,10 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///babies.sqlite"
-
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///babies.sqlite"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
 db = SQLAlchemy(app)
-
 
 class Baby (db.Model):
     __tablename__ = 'babies'
